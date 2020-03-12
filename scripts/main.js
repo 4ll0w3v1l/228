97f86f617d9cd2b856cd08ed563ba28e5154eb60
RUr1ck <betahellatester@gmail.com>
2020-03-12 06:36:49 +0300

    starting

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+	<script>
+		
+		// var name = 'cHris diXon';
+
+		// var split = name.split(" ");
+		// var name1 = split[0];
+		// var name2 = split[1];
+		// var formattedName1 = name1.slice(0,1).toUpperCase() + name1.slice(1).toLowerCase();
+		// var formattedName2 = name2.slice(0,1).toUpperCase() + name2.slice(1).toLowerCase();
+		// var formattedName = formattedName1 + ' ' + formattedName2;
+		// document.write(formattedName);
+
+		var x = Math.random();
+		var y = x*100
+		alert(Math.round(y));
+
+	</script>
